77e0f8e940cb3fe43faf86eb5388c3c9370af341
Администратор <slegionv@yandex.ru>
2020-03-05 15:56:30 +0700

    Added file "Готовимся к миссии"

--- Готовимся к миссии.py
+++ Готовимся к миссии.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('index.html', title=title)
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
